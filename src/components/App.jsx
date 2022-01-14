9e8f5217268d53154ab00ee79326622d6e72dca7
Anna Karolyne <dev.akvc@gmail.com>
2022-01-14 17:00:34 -0300

    Component com contador

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,30 +11,36 @@ import CondicionalComIf from "./CondicionalComIf";
 import Pai from "./comunicacao/direta/Pai";
 import Super from "./comunicacao/indireta/Super";
 import Input from "./form/Input"
+import Contador from "./contador/Contador";
 
 export default(props) => (
     <div className="App">
-        <Card titulo="Card 1" color="#CCCCFF"><Primeiro/></Card>
-        <Card titulo="Card 2">
-            <ComParametro titulo="Meu título" subtitulo="O subtítulo"/>
-        </Card>
-        <Card titulo="Card 3" color="#CCCCFF">
-            <ComFilhos>
-                <ul>
-                    <li>Puguinho</li>
-                    <li>Pug baby</li>
-                    <li>Pug neném</li>
-                </ul>
-            </ComFilhos>
-        </Card>
-        <Card titulo="Card 4">
-            Conteúdo do card
-        </Card>
-        <Card titulo="Card 5" color="#CCCCFF"><Repeticao/></Card>
-        <Card titulo="Card 6"><Condicional numero={14}/></Card>
-        <Card titulo="Card 7" color="#CCCCFF"><CondicionalComIf numero={7}/></Card>
-        <Card titulo="Card 8"><Pai sobrenome="Ribeiro"/></Card>
-        <Card titulo="Card 9" color="#CCCCFF"><Super/></Card>
-        <Card titulo="Card 10"><Input/></Card>
+        <h1>Fundamentos React</h1>
+
+        <div className="Cards">
+            <Card titulo="Card 1" color="#CCCCFF"><Primeiro/></Card>
+            <Card titulo="Card 2">
+                <ComParametro titulo="Meu título" subtitulo="O subtítulo"/>
+            </Card>
+            <Card titulo="Card 3" color="#CCCCFF">
+                <ComFilhos>
+                    <ul>
+                        <li>Puguinho</li>
+                        <li>Pug baby</li>
+                        <li>Pug neném</li>
+                    </ul>
+                </ComFilhos>
+            </Card>
+            <Card titulo="Card 4">
+                Conteúdo do card
+            </Card>
+            <Card titulo="Card 5" color="#CCCCFF"><Repeticao/></Card>
+            <Card titulo="Card 6"><Condicional numero={14}/></Card>
+            <Card titulo="Card 7" color="#CCCCFF"><CondicionalComIf numero={7}/></Card>
+            <Card titulo="Card 8"><Pai sobrenome="Ribeiro"/></Card>
+            <Card titulo="Card 9" color="#CCCCFF"><Super/></Card>
+            <Card titulo="Card 10"><Input/></Card>
+            <Card titulo="Card 11" color="#CCCCFF"><Contador passo={10} valor={100}/></Card>
+        </div>
     </div>
 )
