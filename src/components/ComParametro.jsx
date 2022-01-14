a4000a7f0b8cf7930bd03060b6072724ef4b89f9
Anna Karolyne <dev.akvc@gmail.com>
2022-01-14 09:25:50 -0300

    Component com parametros

---
+++ src/components/ComParametro.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import react from "react";
+
+export default param =>
+
+<>
+    <h3>{param.titulo}</h3>
+    <p>{param.subtitulo}</p>
+</>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import reactDom from 'react-dom'
+import ComParametro from './components/ComParametro'
 import Primeiro from './components/Primeiro'
 import './index.css'
 
+
 reactDom.render(
-    <Primeiro/>,
+    <div>
+        <Primeiro/>
+        <ComParametro titulo="Meu título" subtitulo="O subtítulo"/>
+    </div>,
     document.getElementById('root')
 )
 
