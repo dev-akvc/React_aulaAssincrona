efcd6cd9f9f64b35730be6ba4d3c619a5a32a071
Anna Karolyne <dev.akvc@gmail.com>
2022-01-14 09:16:05 -0300

    Primeiro component

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default () =>
+    <>
+        <h1>Meu site</h1>
+        <p>Aqui é tudo hotpink</p>
+    </>
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import reactDom from 'react-dom'
+import Primeiro from './components/Primeiro'
+import './index.css'
+
+reactDom.render(
+    <Primeiro/>,
+    document.getElementById('root')
+)
+
